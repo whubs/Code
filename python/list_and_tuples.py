3d02c560d7b7e55c341bd42c80dce1623e8068a2
whubs <fullstackcoder101@gmail.com>
2018-02-17 01:47:36 +0000

    List and tuples sequences overview

--- python/list_and_tuples.py
+++ python/list_and_tuples.py
@@ -1,7 +1,24 @@
 #! /usr/bin/python
 
 # Sequence Overview
+
 ed = ['Edward Gumby', 32];
 john = ['John Smith', 30];
 database0 = [ed, john];
 print database0;
+print "\n";
+
+# Sequence Operations
+
+hi = 'Hello';
+print hi[0];
+print hi[-1];
+print hi[1];
+print "\n";
+
+print 'Hello'[1];
+print "\n";
+
+fourth = raw_input('Year: ')[3];
+print fourth;
+print "\n";
