540f6954c00fd000ca6b23c911818e73e3f8702f
whubs <fullstackcoder101@gmail.com>
2018-02-17 02:07:19 +0000

    Index example program that outputs a date

---
+++ python/index_ex_prog.py
@@ -0,0 +1,37 @@
+# Print out a date, given year, month, and day as numbers
+
+months = [
+'January', 
+'February', 
+'March', 
+'April', 
+'May', 
+'June', 
+'July', 
+'August', 
+'September', 
+'October', 
+'November', 
+'December'
+];
+
+# A list with one ending for each number from 1 to 31
+
+f3 = ['st', 'nd', 'rd'];
+th = ['th'];
+
+endings = f3 + 17 * th + f3 + 7 * th + f3[0];
+
+year = raw_input('Year: ');
+month = raw_input('Month (1-12): ');
+day = raw_input('Day (1-31): ');
+
+month_num = int(month);
+day_num = int(day);
+
+# Subtract 1 from month and day to get a correct index
+
+month_name = months[month_num - 1];
+day_end = day + endings[day_num - 1];
+
+print month_name + ' ' + day_end + ', ' year;
