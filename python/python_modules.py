2799007206316ec80b807752af6b37f3ef09e34a
whubs <fullstackcoder101@gmail.com>
2018-02-17 00:13:09 +0000

    Some python modules (math)

---
+++ python/python_modules.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python
+
+#from math import sqrt
+import math
+
+print math.floor(32.9)
+print int(math.floor(32.9))
+#print sqrt(9) if i uncomment line 3
+print math.sqrt(9)
+print int(math.sqrt(9))
+
+# I can assign functions to varialbes, for example:
+# var1 = math.sqrt
+# var1(9)
+# = 3
