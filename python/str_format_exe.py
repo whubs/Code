913128dc269e746ba46f620d546f7ca5aff4db3f
whubs <fullstackcoder101@gmail.com>
2018-02-21 18:13:15 +0000

    Ended Receipt program

--- python/str_format_exe.py
+++ python/str_format_exe.py
@@ -16,4 +16,10 @@ print header_format % (item_width, 'Item', price_width, 'Price');
 
 print '-' * width;
 
-print format % (item_width, 'A')
+print format % (item_width, 'Apples', price_width, 0.4);
+print format % (item_width, 'Pears', price_width, 0.5);
+print format % (item_width, 'Cantaloupes', price_width, 1.92);
+print format % (item_width, 'Dried Apricots (16 oz.)', price_width, 8);
+print format % (item_width, 'Prunes (4 lbs.)', price_width, 12);
+
+print '=' * width;
