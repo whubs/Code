6623e86bdfd51714c2fb40e177adcc9276aa7b4f
whubs <fullstackcoder101@gmail.com>
2018-02-27 16:33:47 +0000

    Introduction to dictionaries

---
+++ python/dictionaries.py
@@ -0,0 +1,26 @@
+# /usr/bin/python
+
+# Dictionaries
+
+# Works but impractical
+names = ['Alice', 'Beth', 'Cecil', 'Dee-Dee', 'Earl'];
+numbers = ['2341', '9102', '3158', '0142', '5551'];
+print numbers[names.index['Cecil']];
+
+# Creating and using dictionaries
+phonebook = {'Alice': '2341', 'Beth':'9102', 'Cecil':'3158', 'Dee-Dee':'0142', 'Earl':'5551'};
+
+# dict Function
+items = [('name', 'Gumby'), ('age', 42)];
+d = dict(items);
+print d;
+print d['name'];
+# OR
+# d = dict(name = 'Gumby', age = 42);
+# print d;
+
+# len(d);
+# d[k];
+# d[k] = v;
+# del d[k];
+# k in d;
