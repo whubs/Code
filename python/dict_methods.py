dc69aeb5ac4b1718cd5f4d897e59cc276828e16e
whubs <fullstackcoder101@gmail.com>
2018-02-27 18:05:24 +0000

    Added  .clear() Dictionary Method

---
+++ python/dict_methods.py
@@ -0,0 +1,32 @@
+# /usr/bin/python
+
+# Dictionary Methods
+d = {};
+d['name'] = 'Gumby';
+d['age'] = 42;
+print d;
+
+returned_value = d.clear();
+print d;
+print '\n';
+
+# Why is this useful?
+# Here's why, scenario 1:
+x = {};
+y = x;
+x['key'] = 'value';
+print y;
+x = {};
+print y;
+# Returns {'key': 'value'}
+print '\n';
+
+# Scenario 2:
+x = {};
+y = x;
+x['key'] = 'value';
+print y;
+x.clear()
+print y;
+# Returns {}
+print '\n';
