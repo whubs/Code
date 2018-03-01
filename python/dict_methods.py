fc059081555f9556a423ab1f0f3cb568f6ef60d5
whubs <fullstackcoder101@gmail.com>
2018-03-01 19:36:24 +0000

    Finished .copy() method and added deepcopy() function

--- python/dict_methods.py
+++ python/dict_methods.py
@@ -35,3 +35,25 @@ print y;
 print '\n';
 
 # .copy() method
+
+x = {'username': 'admin', 'machines': ['foo', 'bar', 'baz']};
+y = x.copy();
+y['username'] = 'mlh';
+y['machines'].remove('bar');
+print y;
+print x;
+
+#  When you replace a value in the copy, the original is unaffected.
+# However, If i modify a value (instead of replacing it),
+# the original is also affected. (for example the list in both y and x),
+# one way to avoid this is to use deepcopy function from the copy module
+
+from copy import deepcopy
+d = {};
+d['names'] = ['Alfred', 'Bertrand'];
+c = d.copy();
+dc = deepcopy(d);
+d['names'].append('Clive');
+print d;
+print c;
+print dc;
